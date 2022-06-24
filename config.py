5b0fe93ab8755fd57df806c934b8b2d9b79b7ee0
aelvismorales <faviotio123@gmail.com>
2022-06-24 17:44:28 -0500

    m

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import urllib.parse
 from flask_sqlalchemy import SQLAlchemy
 
 class Config(object):
-    SECRET_KEY='asd123'
+    SECRET_KEY='123447a47f563e90fe2db0f56b1b17be62378e31b7cfd3adc776c59ca4c75e2fc512c15f69bb38307d11d5d17a41a7936789'
 
 class DevelopmentConfig(Config):
     DEBUG=True
